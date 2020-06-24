8197ae255ff7218afdbe9614ac669d8986d90a57
micha3lb3n <michaelben7781@gmail.com>
2020-06-24 12:28:39 +0530

    More interactive design

--- subdomains.sh
+++ subdomains.sh
@@ -1,3 +1,11 @@
+black=`tput setaf 0`
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+reset=`tput sgr0`
+
 while getopts "d:" opt
 do
 	case "${opt}" in
@@ -23,37 +31,51 @@ if [[ ! -d ~/.subdomains/$target ]]; then
 	mkdir ~/.subdomains/$target
 fi
 
-
+echo -e "\n${yellow}[+] Starting subfinder${reset}\n"
 if [[ ! -f $location/subfinder-$target ]]; then
 	subfinder -d $target | tee $location/subfinder-$target
+	echo -e "\n${green}[-] Subfinder done${reset}"
 else
-	echo -e "Subfinder already done\n"
+	echo -e "${cyan}Subfinder already done${reset}"
 fi
+
+echo -e "\n${yellow}[+] Starting amass${reset}\n"
 
 if [[ ! -f $location/amass-$target ]]; then
 	amass enum -d $target -o $location/amass-$target
+	echo -e "\n${green}[-] Amass done${reset}"
 else
-	echo -e "Amass already done\n"
+	echo -e "${cyan}Amass already done${reset}"
 fi
+
+echo -e "\n${yellow}[+] Starting findomain${reset}\n"
 
 if [[ ! -f $location/findomain-$target ]]; then
 	findomain -t $target -u $location/findomain-$target
+	echo -e "\n${green}[-] Findomain done${reset}"
 else
-	echo -e "Findomain already done\n"
+	echo -e "${cyan}Findomain already done\n${reset}"
 fi
 
+echo -e "\n${yellow}[+] Starting assetfinder${reset}\n"
+
 if [[ ! -f $location/assetfinder-$target ]]; then
-	assetfinder $target | tee $location/assetfinder-$target
+	assetfinder -subs-only $target | tee $location/assetfinder-$target
+	echo -e "\n${green}[-] Assetfinder done${reset}"
 else
-	echo -e "Assetfinder already done\n"
+	echo -e "${cyan}Assetfinder already done\n${reset}"
 fi
+
+echo -e "\n${yellow}[+] Starting subdomainizer${reset}\n"
 
 if [[ ! -f $location/subdomainizer-$target ]]; then
 	python3 ~/tools/SubDomainizer/SubDomainizer.py -u $target -g -gt f236a11e6591955df4b985b83ead43eff4f9bd5e -o $location/subdomainizer-$target
+	echo -e "\n${green}[-] Subdomainizer done${reset}"
 else
-	echo -e "Subdomainizer already done\n"
+	echo -e "${cyan}Subdomainizer already done${reset}\n"
 fi
 
+echo -e "\n\n${green}The final list of subdomains are:\n${yellow}"
 cat $location/* | sort -u | tee $location/$target-subdomains
-
-
+echo "${reset}"
+sed 's/^/http:\/\//' -i $location/$target-subdomains > $location/http-$target-subdomains
