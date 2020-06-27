2e42c6fe8cf5c82d62c5d248446f8b8b1c3d2f78
micha3lb3n <michaelben7781@gmail.com>
2020-06-27 14:33:17 +0530

    Bug fixes

--- subdomains.sh
+++ subdomains.sh
@@ -23,18 +23,20 @@ fi
 
 if [[ $target == "list" ]]; then
 	targets=$(ls ~/.recon-data/)
+	echo ""
 	for t in $targets; do
 		if [[ -f ~/.recon-data/$t/subdomains/$t-subdomains ]]; then
 			l=$(cat ~/.recon-data/$t/subdomains/$t-subdomains | grep -c "")
 			echo "$t - $l subdomains"
 		else
-			echo "${red}$t scanning not complete${reset}"
+			echo "${red}$t - scanning not complete${reset}"
 		fi
 	done
+	echo ""
 	exit 2
 fi
 
-location=~/.recon-data/$target/subdomains
+subLocation=~/.recon-data/$target/subdomains
 
 if [[ -d ~/.recon-data/$target/subdomains ]]; then
 	read -p "The target's data already exists, do you want to start fresh, if no it continues from where you left(y/n): " input
@@ -51,8 +53,8 @@ if [[ ! -d ~/.recon-data/$target/subdomains ]]; then
 fi
 
 echo -e "\n${yellow}[+] Starting subfinder${reset}\n"
-if [[ ! -f $location/subfinder-$target ]]; then
-	subfinder -d $target | tee $location/subfinder-$target
+if [[ ! -f $subLocation/subfinder-$target ]]; then
+	subfinder -d $target | tee $subLocation/subfinder-$target
 	echo -e "\n${green}[-] Subfinder done${reset}"
 else
 	echo -e "${cyan}Subfinder already done${reset}"
@@ -60,8 +62,8 @@ fi
 
 echo -e "\n${yellow}[+] Starting amass${reset}\n"
 
-if [[ ! -f $location/amass-$target ]]; then
-	amass enum --passive -d $target -o $location/amass-$target
+if [[ ! -f $subLocation/amass-$target ]]; then
+	amass enum --passive -d $target -o $subLocation/amass-$target
 	echo -e "\n${green}[-] Amass done${reset}"
 else
 	echo -e "${cyan}Amass already done${reset}"
@@ -69,8 +71,8 @@ fi
 
 echo -e "\n${yellow}[+] Starting findomain${reset}\n"
 
-if [[ ! -f $location/findomain-$target ]]; then
-	findomain -t $target -u $location/findomain-$target
+if [[ ! -f $subLocation/findomain-$target ]]; then
+	findomain -t $target -u $subLocation/findomain-$target
 	echo -e "\n${green}[-] Findomain done${reset}"
 else
 	echo -e "${cyan}Findomain already done\n${reset}"
@@ -78,8 +80,8 @@ fi
 
 echo -e "\n${yellow}[+] Starting assetfinder${reset}\n"
 
-if [[ ! -f $location/assetfinder-$target ]]; then
-	assetfinder -subs-only $target | tee $location/assetfinder-$target
+if [[ ! -f $subLocation/assetfinder-$target ]]; then
+	assetfinder -subs-only $target | tee $subLocation/assetfinder-$target
 	echo -e "\n${green}[-] Assetfinder done${reset}"
 else
 	echo -e "${cyan}Assetfinder already done\n${reset}"
@@ -87,16 +89,16 @@ fi
 
 echo -e "\n${yellow}[+] Starting chaos${reset}\n"
 
-if [[ ! -f $location/chaos-$target ]]; then
-	chaos -d $target | tee $location/chaos-$target
+if [[ ! -f $subLocation/chaos-$target ]]; then
+	chaos -d $target | tee $subLocation/chaos-$target
 else
 	echo -e "${cyan}Chaos already done\n${reset}"
 fi
 
 echo -e "\n${yellow}[+] Starting subdomainizer${reset}\n"
 
-if [[ ! -f $location/subdomainizer-$target ]]; then
-	python3 ~/tools/SubDomainizer/SubDomainizer.py -u $target -g -gt $GITHUB_API_KEY -o $location/subdomainizer-$target
+if [[ ! -f $subLocation/subdomainizer-$target ]]; then
+	python3 ~/tools/SubDomainizer/SubDomainizer.py -u $target -g -gt $GITHUB_API_KEY -o $subLocation/subdomainizer-$target
 	echo -e "\n${green}[-] Subdomainizer done${reset}"
 else
 	echo -e "${cyan}Subdomainizer already done${reset}\n"
@@ -104,27 +106,57 @@ fi
 
 echo -e "\n${yellow}[+] Starting github subdomains\n${reset}"
 
-if [[ ! -f $location/github-subdomains-$target ]]; then
-	python3 ~/tools/github-search/github-subdomains.py -d $target -t $GITHUB_TOKEN | tee $location/github-subdomains-$target
+if [[ ! -f $subLocation/github-subdomains-$target ]]; then
+	python3 ~/tools/github-search/github-subdomains.py -d $target -t $GITHUB_TOKEN | tee $subLocation/github-subdomains-$target
 	echo -e "\n${green}[-] Github subdomains done${reset}"
 else
 	echo -e "\n${cyan}Github subdomains already done${reset}\n"
 fi
 
-if [[ ! -d $location/crawl-data ]]; then
+echo -e "\n\n${green}The final list of subdomains are:\n${yellow}"
+cat $subLocation/* | sort -u | grep $target | tee $subLocation/$target-subdomains
+echo "${reset}"
+total=$(cat $subLocation/$target-subdomains | grep -c "")
+echo -e "\n\nTotal subdomains found: $total\n"
+
+domainHeart=~/.recon-data/$target
+
+
+
+if [[ ! -d $domainHeart/crawl-data ]]; then
 
 	read -p "Should I run the linkfinding module [y/n]: " input
-
+	
 	if [[ $input == 'Y' || $input == 'y' ]]; then
-		gospider -s $location/
+		if [[ -f $subLocation/$target-subdomains ]]; then
+			echo -e "\n${yellow}[+] Starting linkfinding${reset}\n"
+			mkdir $domainHeart/crawl-data
+			~/tools/bash-hacks/./add-http.sh $subLocation/$target-subdomains > $subLocation/http-$target-subdomains
+			gospider --sites $subLocation/http-$target-subdomains -t 20 -c 10 --include-subs --include-other-source -o $domainHeart/crawl-data
+			cat $domainHeart/crawl-data/* | sort -u > $domainHeart/crawl-data/final-results
+			echo -e "\n${yellow}The subdomains found crawling are: \n${reset}"
+			cat $domainHeart/crawl-data/* | sort -u | grep subdomains | cut -d - -f 2 | cut -d " " -f 2 | tee $subLocation/crawl-subdomains
+			rm $subLocation/http-$target-subdomains
+			cat $subLocation/* | sort -u > $subLocation/$target-subdomains
+			echo -e "\n${green}[-] Linkfinding done${reset}\n"
+		else
+			echo "${red}Please complete the subdomain scan first!${reset}\n"
+		fi
 	fi
 fi
 
-echo -e "\n\n${green}The final list of subdomains are:\n${yellow}"
-cat $location/* | sort -u | grep $target | tee $location/$target-subdomains
-echo "${reset}"
-total=$(cat $location/$target-subdomains | grep -c "")
-echo -e "\n\nTotal subdomains found: $total\n"
+if [[ ! -f $domainHeart/ports ]]; then
+	read -p "Should I port scan [y/n]: " input
 
+	if [[ $input == 'y' ]]; then
+		if [[ -f $subLocation/$target-subdomains ]]; then
+			echo -e "\n${yellow}[+] Starting port scan ${reset}\n"
+			naabu -hL $subLocation/$target-subdomains -t 20 -o $domainHeart/ports
+			echo -e "\n${green}[-] Port scanning completed${reset}\n"
+		else
+			echo "${red}Please complete the subdomain scan first!${reset}\n"
+		fi
+	fi
+fi
 
 
